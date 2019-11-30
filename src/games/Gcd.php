cdaf064cc84da1871058bd577e2cd454ec0cc11b
elvolt <sidelevCS@gmail.com>
2019-11-30 14:07:58 +0300

    Change function findGCD

--- src/games/Gcd.php
+++ src/games/Gcd.php
@@ -8,21 +8,14 @@ const TASK = 'Find the greatest common divisor of given numbers.';
 const MIN_NUM = 1;
 const MAX_NUM = 100;
 
-function findNOD(int $num1, int $num2): int
+function findGCD(int $num1, int $num2): int
 {
-    $nod = 0;
-    if (($num1 === 1) || ($num2 === 1)) {
-        $nod = 1;
-    }
+    $max = max($num1, $num2);
     $min = min($num1, $num2);
-    for ($i = $min; $i >= 1; $i -= 1) {
-        if (($num1 % $i === 0) && ($num2 % $i === 0)) {
-            $nod = $i;
-            break;
-        }
-    }
 
-    return $nod;
+    $reminder = $max % $min;
+
+    return ($reminder === 0) ? $min : findGCD($min, $reminder);
 }
 
 function launchGame(): void
@@ -31,7 +24,7 @@ function launchGame(): void
         $num1 = mt_rand(MIN_NUM, MAX_NUM);
         $num2 = mt_rand(MIN_NUM, MAX_NUM);
 
-        $rightAnswer = findNOD($num1, $num2);
+        $rightAnswer = findGCD($num1, $num2);
         $question = "{$num1} {$num2}";
         return [$question, $rightAnswer];
     };
